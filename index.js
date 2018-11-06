d4f0fd6ac92f668d2a5776aff4a6edd428275586
Vanessa Prema <vanessabprema@gmail.com>
2018-11-06 18:06:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians,instruments) {
+  let beatles = [ ];
+  for (let i=0; i<musicians.length; i++){
+    const str = musicians[i] + " plays " + instruments[i];
+    beatles.push(str);
+  }
+  return beatles
+}
